6dec2aef4d431f7602076aef0538db1c80b21cdf
Robley Evans <rde5329@g.rit.edu>
2020-02-08 15:35:42 -0500

    Started the beginning of the program

---
+++ RecordKeeperProgram/RecordKeeperProgram/CredentialLoader.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+/// <summary>
+/// AUTHOR: Robley Evans
+/// PURPOSE: To load in the usernames and 
+/// passwords so that people can login. It
+/// will also be able to save updated or new
+/// users into the files
+/// </summary>
+namespace RecordKeeperProgram
+{
+    class CredentialLoader
+    {
+        // streams and readers
+        private BinaryReader binaryReader;
+        private BinaryWriter binaryWriter;
+        private const string path = @"C:\Record Keeper";
+        private FileStream stream;
+        // private ASCIIEncoding encoder;
+
+        // Dictionary for usernames and such
+        private Dictionary<string, string> users;
+
+        // PROPERTIES
+        public Dictionary<string, string> Users
+        {
+            get { return users; }
+            set { users = value; }
+        }
+
+        // CONSTRUCTOR
+        public CredentialLoader()
+        {
+            users = new Dictionary<string, string>();
+            users.Add("ADMIN", "YELBOR3120");
+        }
+
+        // METHODS
+        public void Start()
+        {
+            try
+            {
+                string credentialsPath = path + "\\Misc Data\\Credentials.data";
+
+                if (Directory.Exists(path))
+                {
+                    binaryReader = new BinaryReader(stream);
+                    stream = File.Open(credentialsPath, FileMode.OpenOrCreate);
+
+                    int numUsers = binaryReader.ReadInt32();
+
+                    for (int i = 0; i < numUsers; i++)
+                    {
+                        users.Add(binaryReader.ReadString(), binaryReader.ReadString());
+                    }
+                }
+                else
+                {
+                    DirectoryInfo di = Directory.CreateDirectory(path);
+                    di = Directory.CreateDirectory(path + "\\Main Data");
+                    di = Directory.CreateDirectory(path + "\\Misc Data");
+                    File.Create(path + "\\Misc Data\\Credentials.data");
+
+                    stream = File.Open(credentialsPath, FileMode.Open);
+                    binaryWriter = new BinaryWriter(stream);
+
+                    binaryWriter.Write(1);
+                    binaryWriter.Write("ADMIN");
+                    binaryWriter.Write("YELBOR3120");
+                    binaryWriter.Close();
+
+                    MessageBox.Show("Directory created!");
+                }
+
+                
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+
+    }
+}
---
+++ RecordKeeperProgram/RecordKeeperProgram/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace RecordKeeperProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginButton = new System.Windows.Forms.Button();
+            this.loginLable = new System.Windows.Forms.Label();
+            this.passTextBox = new System.Windows.Forms.TextBox();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginButton
+            // 
+            this.loginButton.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(284, 244);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(100, 25);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "Submit";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // loginLable
+            // 
+            this.loginLable.AutoSize = true;
+            this.loginLable.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginLable.Location = new System.Drawing.Point(280, 75);
+            this.loginLable.Name = "loginLable";
+            this.loginLable.Size = new System.Drawing.Size(62, 24);
+            this.loginLable.TabIndex = 1;
+            this.loginLable.Text = "Login";
+            // 
+            // passTextBox
+            // 
+            this.passTextBox.Location = new System.Drawing.Point(284, 197);
+            this.passTextBox.Name = "passTextBox";
+            this.passTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passTextBox.TabIndex = 2;
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameLabel.Location = new System.Drawing.Point(183, 144);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(73, 19);
+            this.usernameLabel.TabIndex = 3;
+            this.usernameLabel.Text = "Username:";
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(284, 143);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userTextBox.TabIndex = 4;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.Location = new System.Drawing.Point(183, 196);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(72, 19);
+            this.passwordLabel.TabIndex = 5;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 356);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.passTextBox);
+            this.Controls.Add(this.loginLable);
+            this.Controls.Add(this.loginButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label loginLable;
+        private System.Windows.Forms.TextBox passTextBox;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.Label passwordLabel;
+    }
+}
+
